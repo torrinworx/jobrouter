68e398be26e1455be2070b2152d18824094ed951
torrin1 <torrin@worx4you.com>
2024-09-21 11:25:45 -0400

    bumping version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jobrouter',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[],
     author='Torrin Leonard',
